763cb76d4d24cd44aa2f51cdebf0b667b1bc0bcb
Derek Miranda <derek.miranda@ff0000.com>
2018-01-09 16:36:53 -0800

    Updated test files

--- test/test.js
+++ test/test.js
@@ -42,12 +42,13 @@ async function fixture(t, entry, options) {
 		});
 	});
 
-	/*
+	/* 
+	// uncomment to update expected files 
 	const bundle = mockFs.readFileSync('/bundle.js', 'utf8');
 	fs.writeFileSync(path.join(__dirname, 'src', 'expected', entry), bundle.replace(/\r/g, ''));
 	const sourcemap = mockFs.readFileSync('/bundle.js.map', 'utf8');
 	fs.writeFileSync(path.join(__dirname, 'src', 'expected', `${entry}.map`), sourcemap.replace(/\\r/g, ''));
-	*/
+	 */
 
 	t.is(
 		normalize(mockFs.readFileSync('/bundle.js', 'utf8')),
