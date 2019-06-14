c44112f34fbec224f50e541e2c5e25789da4051d
Derek Miranda <derek.miranda@ff0000.com>
2019-06-14 12:00:30 -0700

    test: updates tests for Webpack 4
    - sets Webpack 4 mode to "none"

--- test/src/expected/external.js
+++ test/src/expected/external.js
@@ -36,12 +36,32 @@
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
 /******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -59,6 +79,7 @@
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
@@ -68,22 +89,21 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__b_js__ = __webpack_require__(1);
-
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _b_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 
 var a = 'a';
-
-var external = a + __WEBPACK_IMPORTED_MODULE_0__b_js__["a" /* default */];
-
+var external = a + _b_js__WEBPACK_IMPORTED_MODULE_0__["default"];
 /* harmony default export */ __webpack_exports__["default"] = (external);
 
 /***/ }),
-/* 1 */
+/* 1 */,
+/* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony default export */ __webpack_exports__["a"] = ('b');
+__webpack_require__.r(__webpack_exports__);
+/* harmony default export */ __webpack_exports__["default"] = ('b');
 
 
 /***/ })
--- test/test.js
+++ test/test.js
@@ -1,122 +1,138 @@
-import path from 'path'
-import fs from 'fs'
-import test from 'ava'
-import webpack from 'webpack'
-import MemoryFS from 'memory-fs'
-import commonjs from 'rollup-plugin-commonjs'
-import { verifyNoES6, writeBabelrc } from './utils'
+import path from "path";
+import fs from "fs";
+import test from "ava";
+import webpack from "webpack";
+import MemoryFS from "memory-fs";
+import commonjs from "rollup-plugin-commonjs";
+import { verifyNoES6, writeBabelrc } from "./utils";
 
 function normalize(string) {
-	return string.replace(/(\r\n|\r|\n)/g, '\n').trim()
+  return string.replace(/(\r\n|\r|\n)/g, "\n").trim();
 }
 
 function setupEnv(entry, options) {
-	const entryPath = path.join(__dirname, 'src', 'fixtures', entry)
-	const compiler = webpack({
-		entry: entryPath,
-		bail: true,
-		output: {
-			path: '/',
-			filename: 'bundle.js'
-		},
-		devtool: 'source-map',
-		module: {
-			rules: [
-				{
-					test: entryPath,
-					use: [
-						{
-							loader: path.join(__dirname, '../index.js'),
-							options
-						}
-					]
-				}
-			]
-		}
-	})
+  const entryPath = path.join(__dirname, "src", "fixtures", entry);
+  const compiler = webpack({
+    mode: "none",
+    entry: entryPath,
+    bail: true,
+    output: {
+      path: "/",
+      filename: "bundle.js"
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: entryPath,
+          use: [
+            {
+              loader: path.join(__dirname, "../index.js"),
+              options
+            }
+          ]
+        }
+      ]
+    }
+  });
 
-	const mockFs = new MemoryFS()
+  const mockFs = new MemoryFS();
 
-	compiler.outputFileSystem = mockFs
+  compiler.outputFileSystem = mockFs;
 
-	return {
-		mockFs,
-		compiler
-	}
+  return {
+    mockFs,
+    compiler
+  };
 }
 
 async function fixture(t, entry, options) {
-	const { mockFs, compiler } = setupEnv(entry, options)
+  const { mockFs, compiler } = setupEnv(entry, options);
 
-	await new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			err ? reject(err) : resolve(stats)
-		})
-	})
+  await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      err ? reject(err) : resolve(stats);
+    });
+  });
 
-	/* 
-	// uncomment to update expected files 
-	const bundle = mockFs.readFileSync('/bundle.js', 'utf8');
-	fs.writeFileSync(path.join(__dirname, 'src', 'expected', entry), bundle.replace(/\r/g, ''));
-	const sourcemap = mockFs.readFileSync('/bundle.js.map', 'utf8');
-	fs.writeFileSync(path.join(__dirname, 'src', 'expected', `${entry}.map`), sourcemap.replace(/\\r/g, ''));
-	 */
+  // // uncomment to update expected files
+  // const bundle = mockFs.readFileSync("/bundle.js", "utf8");
+  // fs.writeFileSync(
+  //   path.join(__dirname, "src", "expected", entry),
+  //   bundle.replace(/\r/g, "")
+  // );
+  // const sourcemap = mockFs.readFileSync("/bundle.js.map", "utf8");
+  // fs.writeFileSync(
+  //   path.join(__dirname, "src", "expected", `${entry}.map`),
+  //   sourcemap.replace(/\\r/g, "")
+  // );
 
-	t.is(
-		normalize(mockFs.readFileSync('/bundle.js', 'utf8')),
-		normalize(fs.readFileSync(path.join(__dirname, 'src', 'expected', entry), 'utf8'))
-	)
+  t.is(
+    normalize(mockFs.readFileSync("/bundle.js", "utf8")),
+    normalize(
+      fs.readFileSync(path.join(__dirname, "src", "expected", entry), "utf8")
+    )
+  );
 
-	t.is(
-		normalize(mockFs.readFileSync('/bundle.js.map', 'utf8')),
-		normalize(fs.readFileSync(path.join(__dirname, 'src', 'expected', `${entry}.map`), 'utf8'))
-	)
+  t.is(
+    normalize(mockFs.readFileSync("/bundle.js.map", "utf8")),
+    normalize(
+      fs.readFileSync(
+        path.join(__dirname, "src", "expected", `${entry}.map`),
+        "utf8"
+      )
+    )
+  );
 }
 
-test('simple', fixture, 'simple.js')
+test("simple", fixture, "simple.js");
 
-test('plugins option', fixture, 'fileLoader.js', { plugins: [commonjs({ extensions: ['.js', '.jpg'] })] })
+test("plugins option", fixture, "fileLoader.js", {
+  plugins: [commonjs({ extensions: [".js", ".jpg"] })]
+});
 
-test('external option', fixture, 'external.js', { external: [path.join(__dirname, 'src', 'b.js')] })
+test("external option", fixture, "external.js", {
+  external: [path.join(__dirname, "src", "b.js")]
+});
 
-test('transpiles ES6 to ES5 w/ Babel settings', async t => {
-	const { mockFs, compiler } = setupEnv('es6_file.js', {
-		babelOptions: {
-			presets: ['@babel/preset-env']
-		}
-	})
+test("transpiles ES6 to ES5 w/ Babel settings", async t => {
+  const { mockFs, compiler } = setupEnv("es6_file.js", {
+    babelOptions: {
+      presets: ["@babel/preset-env"]
+    }
+  });
 
-	await new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			err ? reject(err) : resolve(stats)
-		})
-	})
+  await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      err ? reject(err) : resolve(stats);
+    });
+  });
 
-	const transpiledSrc = mockFs.readFileSync('/bundle.js', 'utf8')
-	verifyNoES6(t, transpiledSrc)
-})
+  const transpiledSrc = mockFs.readFileSync("/bundle.js", "utf8");
+  verifyNoES6(t, transpiledSrc);
+});
 
 test.after("Reads importing package's .babelrc if no babelOptions", async t => {
-	const { mockFs, compiler } = setupEnv('es6_file.js')
-	const babelPath = path.resolve(__dirname, '.babelrc')
+  const { mockFs, compiler } = setupEnv("es6_file.js");
+  const babelPath = path.resolve(__dirname, ".babelrc");
 
-	fs.writeFileSync(
-		babelPath,
-		`
+  fs.writeFileSync(
+    babelPath,
+    `
 	{
 		"presets": ["@babel/preset-env"]
 	}	
 	`
-	)
+  );
 
-	await new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			err ? reject(err) : resolve(stats)
-		})
-	})
+  await new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      err ? reject(err) : resolve(stats);
+    });
+  });
 
-	fs.unlinkSync(babelPath)
+  fs.unlinkSync(babelPath);
 
-	const transpiledSrc = mockFs.readFileSync('/bundle.js', 'utf8')
-	verifyNoES6(t, transpiledSrc)
-})
+  const transpiledSrc = mockFs.readFileSync("/bundle.js", "utf8");
+  verifyNoES6(t, transpiledSrc);
+});
